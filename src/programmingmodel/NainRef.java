9f780a2ff873f8dc7155a77842cf46a47281b218
Eslem <j.eslem03@gmail.comm>
2016-07-02 21:17:54 +0200

    to linux

---
+++ src/programmingmodel/NainRef.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programmingmodel;
+
+/**
+ *
+ * @author eslem
+ */
+class _NainRef {
+
+    private int count = 0;
+    private String id;
+
+    _NainRef(String id) {
+        this.id = id;
+    }
+
+    public void increment() {
+        this.count++;
+    }
+
+    public String toString() {
+        return this.id + " " + this.count;
+    }
+
+}
+
+public class NainRef {
+
+    public static void increment(_NainRef ref) {
+        ref.increment();
+    }
+
+    public static void main(String[] args) {
+        _NainRef one = new _NainRef("One");
+        one.increment();
+        System.out.println(one);
+        _NainRef two = one;
+        two.increment();
+        System.out.println(one);
+        System.out.println(two);
+
+        _NainRef method = new _NainRef("Method");
+        System.out.println(method);
+        increment(method);
+
+        System.out.println(method);
+
+        String string1 = "hello";
+        String string2 = string1;
+
+        string1 = "world";
+        StdOut.println(string1);
+        StdOut.println(string2);
+
+    }
+
+}
